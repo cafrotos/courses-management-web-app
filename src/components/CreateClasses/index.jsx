3c0cf6b68e75b75c324b3f7e41fb75532022aee1
minhphuong <nguyenminhphuong@unimarmores.com>
2019-04-19 10:37:59 +0700

    save change

---
+++ src/components/CreateClasses/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Layout } from 'antd'
+
+class CreateClasses extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <Layout>
+
+      </Layout>
+    )
+  }
+}
+
+export default CreateClasses;
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Courses from './Courses/index'
 import CoursesLayout from './CoursesLayout'
 import PrivateRoute from './Route/PrivateRoute'
 import CustomRoute from './Route/CustomRoute'
+import CreateClasses from './CreateClasses'
 
 export {
   Header,
   Courses,
   CoursesLayout,
   PrivateRoute,
-  CustomRoute
+  CustomRoute,
+  CreateClasses
 }
