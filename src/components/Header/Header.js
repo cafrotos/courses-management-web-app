cad2a594d35c802f050414d0ec7c2edcd1d54c68
DuongVNU <duongk22899@gmail.com>
2019-05-05 14:19:17 +0700

    abc

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,40 @@
 import React from 'react'
-import { Layout, Row, Col, Input, Icon } from "antd";
+import {Layout, Row, Col, Input, Icon} from "antd";
 import MenuUser from './MenuUser'
 import "./header.less"
 import {withRouter} from "react-router-dom";
 
 
-
-const { Header } = Layout;
+const {Header} = Layout;
 
 class Dashboard extends React.Component {
   render() {
-    let { toggle, collapsed } = this.props;
+    let {toggle, collapsed} = this.props;
     return (
-        <Header className="header">
-          <Row align="bottom">
-            <Col span={6} className="header__title__left">
-              <Icon type={collapsed ? "menu-unfold" : "menu-fold"} onClick={toggle} />
-              <span className="title">
+      <Header className="header">
+        <Row align="bottom">
+          <Col span={6} className="header__title__left">
+            <Icon type={collapsed ? "menu-unfold" : "menu-fold"} onClick={toggle}/>
+            <span className="title">
             Quản lý môn học
           </span>
-            </Col>
-            {/* <Col span={3} style={{height: '', backgroundColor: 'red'}}></Col>
+          </Col>
+          {/* <Col span={3} style={{height: '', backgroundColor: 'red'}}></Col>
         <Col span={3} style={{height: '', backgroundColor: 'green'}}></Col>
         <Col span={3} style={{height: '', backgroundColor: 'blue'}}></Col> */}
-            <Col span={8} className="header__title__right">
-              <Input
-                  className="header__search"
-                  placeholder="Tìm kiếm lớp"
-                  prefix={<Icon type="search" />}
-              />
-              <Icon type="plus" className="create__class" />
-              <span>
-            <MenuUser />
+          <Col span={8} className="header__title__right">
+            <Input
+              className="header__search"
+              placeholder="Tìm kiếm lớp"
+              prefix={<Icon type="search"/>}
+            />
+            <Icon type="plus" className="create__class"/>
+            <span>
+            <MenuUser/>
           </span>
-            </Col>
-          </Row>
-        </Header>
+          </Col>
+        </Row>
+      </Header>
     )
   }
 }
